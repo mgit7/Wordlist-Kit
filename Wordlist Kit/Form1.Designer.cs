db9b93a0a5b72e859d891b157458ac0f4e24fedb
mgit7 <mgithub7@gmail.com>
2018-04-25 17:32:09 -0700

    first commit

---
+++ Wordlist Kit/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PA_4_Gelyana_Vara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(211, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(61, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display Wordlist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(61, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Display Rhyming Words";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Display Scrabble Words";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Type in your word below:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 26);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Display Morph Words";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 315);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WordList Kit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Wordlist Kit/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA_4_Gelyana_Vara
+{
+    public partial class Form1 : Form
+    {
+        String[] dictionary = Properties.Resources.WordList.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.BeginUpdate();
+            listBox1.Items.Clear();
+            foreach (string word in dictionary)
+                listBox1.Items.Add(word);
+            listBox1.EndUpdate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string rhyme = textBox1.Text;  //grabs text inputted from text box
+            int count = 0;
+            listBox1.Items.Clear();
+            listBox1.BeginUpdate();
+            foreach (string word in dictionary)
+            {
+                if (word.EndsWith(rhyme))
+                {
+                    listBox1.Items.Add(word);
+                }
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string scrabble = textBox1.Text;
+            string tempWord;
+            int containsScrabble = 0;
+            int wordIndex = 0;
+
+
+            //The outter foreach loop takes in each line of words one by one from the List 
+            //individually and stores them in the variable 'words' which also stores them in 
+            //'tempWord'. The inner foreach loop will check through all letters in the string 
+            //'scrabble' against the words in 'tempword' from the text file to find words that 
+            //can be made from the string of characters. 
+            listBox1.Items.Clear();
+            listBox1.BeginUpdate();
+            foreach (string words in dictionary)
+            {
+                tempWord = words;
+                foreach (char letter in scrabble)
+                {
+
+                    //If the Word from the text file contains a letter from the 'scrabble' string,
+                    //'containsScrabble' will act as a counter for the amount of letters that match.
+                    // Also if the letter matches it will be deleted from 'tempword'. This takes care 
+                    // of a letter being counted twice in a word.  
+                    if (tempWord.Contains(letter))
+                    {
+                        containsScrabble = containsScrabble + 1;
+                        wordIndex = tempWord.IndexOf(letter);
+                        tempWord = tempWord.Remove(wordIndex, 1);
+                    }
+                }
+
+                //A word will only be printed if the 'word' length is equal to the amount 
+                //of letters matched counted by 'containsScrabble' and the word length is greater 
+                //than 3. This makes sure that the the entire 'word' is created from the 
+                //'scrabble' letters.
+
+                if (containsScrabble == words.Length && (words.Length >= 3))
+                {
+                    listBox1.Items.Add(words);
+                }
+
+                //Resets counter
+                containsScrabble = 0;
+
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string morphWord;
+            string tempWord;
+            int containsMorphWord = 0;
+
+            //The while loop takes in each line of words from the text file individually 
+            //and stores them in the variable 'word' which will iterate through
+            //the wordlist. The foreach loop runs through each letter in the string
+            //'morphword' to find words that disagree by one letter.
+            listBox1.Items.Clear();
+            listBox1.BeginUpdate();
+            morphWord = textBox1.Text;
+            foreach (string words in dictionary)
+            {
+                //If the morphWord length does not equal the word lenth we can 
+                //skip the word.
+                if (morphWord.Length == words.Length)
+                {
+                    tempWord = words;
+
+                    foreach (char letter in morphWord)
+                    {
+                        if (tempWord.StartsWith(letter.ToString()))
+                        {
+                            containsMorphWord = containsMorphWord + 1;
+                        }
+
+                        //Each letter is removed after being checked to ensure it is not counted twice
+                        tempWord = tempWord.Remove(0, 1);
+                    }
+
+                    //If 'containsMorphWord' counter is equal to the 'word'- 1 then the 'word' disagrees by one letter
+                    //from the morphWord therefore it is printed to the console.
+                    if ((containsMorphWord == (words.Length - 1)))
+                    {
+                        listBox1.Items.Add(words);
+                    }
+                }
+
+                //Reset counter
+                containsMorphWord = 0;
+            }
+            listBox1.EndUpdate();
+        }
+    }
+}
